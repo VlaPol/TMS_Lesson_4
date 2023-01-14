b260d352a8e855f5f4d47df7ab34c8df1d0d1a8a
pva <tango2002@gmail.com>
2023-01-14 08:50:14 +0300

    feat: first task finished

--- src/ThreeDimensionArray.java
+++ src/ThreeDimensionArray.java
@@ -1,25 +1,61 @@
 import java.util.Random;
+import java.util.Scanner;
 
+/**
+ * Создать трехмерный массив из целых чисел.
+ * С помощью циклов "пройти" по всему массиву и увеличить каждый
+ * элемент на заданное число. Пусть число, на которое будет
+ * увеличиваться каждый элемент, задается из консоли.
+ */
 public class ThreeDimensionArray {
 
     public static void main(String[] args) {
-        ThreeDimensionArray.createThreeDiArray(2,3,4);
+
+        int xDim;
+        int yDim;
+        int zDim;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размерность массива. Для наглядности" +
+                " значения должны быть больше 1 ");
+      do {
+          System.out.print("Первое значение: ");
+          xDim = scanner.nextInt();
+          System.out.print("Второе значение: ");
+          yDim = scanner.nextInt();
+          System.out.print("Значение значение: ");
+          zDim = scanner.nextInt();
+          if(xDim <=1 || yDim <= 1 || zDim <= 1) System.out.println("Введены некорректные исходные данные. " +
+                  "Для наглядности ВСЕ значения должны быть больше 1 !");
+      }while( xDim <= 1 || yDim <= 1 || zDim <= 1);
+
+        System.out.println("*******   Исходные массивы  *******");
+        int [][][] preparedArray  = createThreeDiArray(xDim, yDim, zDim);
+        printArray(preparedArray);
+        System.out.println("***********************************");
+
+        System.out.print("Введите число, на которое необходимо увеличить каждый элемент массива: ");
+        int addNUmber = scanner.nextInt();
+
+        System.out.println("\n*****   Измененный массивы  ******");
+        printArray(task(preparedArray,addNUmber));
+        System.out.println("***********************************");
+
     }
 
-    private static void createThreeDiArray(int x, int y, int z){
+    private static int [][][] createThreeDiArray(int x, int y, int z){
 
         int [][][] threeDArray = new int[x][y][z];
 
         for(int i=0; i< threeDArray.length; i++){
             for(int j = 0; j < threeDArray[i].length; j++){
                 for (int k = 0; k < threeDArray[i][j].length; k++){
-                    int number = new Random().nextInt(20);
-                    threeDArray[i][j][k] = number;
+                    threeDArray[i][j][k] = new Random().nextInt(20);
                 }
             }
         }
 
-        printArray(threeDArray);
+        return threeDArray;
     }
 
     private static void printArray(int [][][] array){
@@ -32,5 +68,16 @@ public class ThreeDimensionArray {
             }
             System.out.println();
         }
+    }
+
+    private static int [][][] task(int [][][] curArray, int number){
+        for(int i=0; i< curArray.length; i++){
+            for(int j = 0; j < curArray[i].length; j++){
+                for (int k = 0; k < curArray[i][j].length; k++){
+                    curArray[i][j][k] += number;
+                }
+            }
+        }
+        return curArray;
     }
 }
